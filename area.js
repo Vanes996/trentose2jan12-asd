40797220a0300e297e1ba18e87b91b5e7252edef
Vanes <vanes996@hotmail.it>
2018-12-18 17:58:58 +0100

    :bug: added a few test cases

--- area.js
+++ area.js
@@ -1,16 +1,20 @@
-
-var array = [14, 2]
-getArea(array)
-
-
 function getArea(array) {
   var l1 = array[0]
   var l2 = array[1]
+  if (array.length != 2) {
+    return -1;
+  }
+  if (l1 == null || l2 == null) {
+    return -1;
+  }
+  if (l1 % 1 !== 0 || l2 % 2 !== 0) { // check if int or float
+    return -1;
+  }
   if (l1 >= 0 && l2 >= 0) {
-    console.log(l1 * l2)
     return l1 * l2;
   } else {
-    console.log(-1)
     return -1;
   }
 }
+
+module.exports = getArea;
---
+++ test/area.test.js
@@ -0,0 +1,102 @@
+const getArea = require('../area.js')
+var array
+
+test('no values', () => {
+  array = []
+  expect(getArea(array)).toBe(-1)
+})
+
+test('[0,2]', () => {
+  array = [0, 2]
+  expect(getArea(array)).toBe(0)
+})
+
+test('[2,2]', () => {
+  array = [2, 2]
+  expect(getArea(array)).toBe(4)
+})
+
+test('3 int', () => {
+  array = [2, 2, 2]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 int', () => {
+  array = [1]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('2 int < 0', () => {
+  array = [-2, -12]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 int,1 int < 0', () => {
+  array = [2, -12]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('2 float', () => {
+  array = [2.2, 2.1]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 int, 1 float', () => {
+  array = [2, 2.1]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('2 char', () => {
+  array = ['a', 'b']
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 int, 1 char', () => {
+  array = [1, 'a']
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 char, 1 int ', () => {
+  array = ['a', 1]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('2 string', () => {
+  array = ["asd", "bsd"]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 int, 1 string', () => {
+  array = [1, "bsd"]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 string, 1 int', () => {
+  array = ["bsd", 1]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('2 null', () => {
+  array = [null, null]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('1 null, 1 int', () => {
+  array = [null, 2]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('incomplete array', () => {
+  array = [1, ]
+  expect(getArea(array)).toBe(-1)
+})
+
+test('object', () => {
+  array = {a:1,b:2}
+  expect(getArea(array)).toBe(-1)
+})
+
+test('two pars instead of one', () => {
+  array=[2, 3], 5
+  expect(getArea(array)).toBe(-1)
+});
