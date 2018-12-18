161a902edbf6cd54faca3c438a2dda69264b754a
Vanes <vanes996@hotmail.it>
2018-12-18 17:11:46 +0100

    asd

---
+++ area.js
@@ -0,0 +1,15 @@
+
+var array = [14, 2]
+getArea(array)
+
+function getArea(array) {
+  var l1 = array[0]
+  var l2 = array[1]
+  if (l1 >= 0 && l2 >= 0) {
+    console.log(l1 * l2)
+    return l1 * l2;
+  } else {
+    console.log(-1)
+    return -1;
+  }
+}
